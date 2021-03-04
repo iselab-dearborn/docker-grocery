e2377a4267bda48874e62ce194b3305297630eb7
Thiago Ferreira <thiagodnf@gmail.com>
2021-03-03 22:10:41 -0500

    Update update-docker-hub.sh

--- update-docker-hub.sh
+++ update-docker-hub.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 cd api
-docker build -t iselabdearborn/docker-grocery-api .
-docker push iselabdearborn/docker-grocery-api
+
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t iselabdearborn/docker-grocery-api --push .
+#docker push iselabdearborn/docker-grocery-api
 cd ..
 
 cd creator
-docker build -t iselabdearborn/docker-grocery-creator .
-docker push iselabdearborn/docker-grocery-creator
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t iselabdearborn/docker-grocery-creator --push .
+#docker push iselabdearborn/docker-grocery-creator
 cd ..
 
 cd web
-docker build -t iselabdearborn/docker-grocery-web .
-docker push iselabdearborn/docker-grocery-web
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t iselabdearborn/docker-grocery-web --push .
+#docker push iselabdearborn/docker-grocery-web
 cd ..
